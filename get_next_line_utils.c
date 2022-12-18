f133d8bd312c35e67bcac5aa6227ed43494e2f1b
Julien Moulines <julienmoulines@MacBook-Pro-de-Julien.local>
2022-12-18 14:57:36 +0100

    ok

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line_utils.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jmouline <jmouline@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jmouline <jul.moulines@free.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/16 23:58:09 by jmouline          #+#    #+#             */
-/*   Updated: 2022/12/17 00:00:21 by jmouline         ###   ########.fr       */
+/*   Updated: 2022/12/18 14:54:39 by jmouline         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ static char	*ft_strjoin_normed(char *s1, char *s2, char *dest)
 	size_t	i;
 
 	if (!dest)
-		return (free(s1), free(s2), NULL);
+		return (NULL);
 	len_s1 = ft_strlen(s1);
 	len_s2 = ft_strlen(s2);
 	i = 0;
@@ -49,12 +49,12 @@ char	*ft_strjoin(char *s1, char *s2)
 	len_s2 = ft_strlen(s2);
 	dest = malloc(len_s1 + len_s2 + 1);
 	if (!dest)
-		return (free(s1), free(s2), NULL);
+		return (NULL);
 	while (i < len_s1 + len_s2 + 1)
 		dest[i++] = '\0';
 	dest = ft_strjoin_normed(s1, s2, dest);
 	if (!dest)
-		return (free(s1), free(s2), NULL);
+		return (NULL);
 	free(s1);
 	return (dest);
 }
